731c75c9d9b0276f98c9785e865a516cb4db23e2
Can4itos <can4ik24862486@gmail.com>
2022-01-09 20:02:53 +0200

    mediumChat

---
+++ routes/chatPg.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var router = express.Router();
+let arr = [];
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+    res.render('chatPg', { title: 'chatPg' });
+});
+router.post('/Message', function(req, res, next) {
+    res.send('chatPg');
+    if(arr.length>4){
+        arr.shift();
+    }
+    arr.push([req.body.name, req.body.message]);
+});
+
+router.get('/Messages', function(req, res, next) {
+    res.json(arr);
+});
+module.exports = router;
+
